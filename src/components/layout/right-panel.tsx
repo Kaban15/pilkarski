be7aa780bca0a37b58ef181768f146575e97d6aa
Kaban <piotrtokeny@gmail.com>
2026-04-16 15:58:22 +0200

    refactor: widen right sidebar from 260px to 320px

--- src/components/layout/right-panel.tsx
+++ src/components/layout/right-panel.tsx
@@ -1,6 +1,6 @@
 export function RightPanel({ children }: { children: React.ReactNode }) {
   return (
-    <aside className="hidden w-[260px] shrink-0 border-l border-border pl-6 lg:block">
+    <aside className="hidden w-[320px] shrink-0 border-l border-border pl-6 lg:block">
       {children}
     </aside>
   );
